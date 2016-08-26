4e4b9abdb90d89aa1064fa07bab630428144a352
Tolga Paksoy <tolga@paramander.com>
2016-08-26 14:30:47 +0200

    Remove JSONP callback function once done

--- src/Native/JsonP.js
+++ src/Native/JsonP.js
@@ -2,11 +2,11 @@ var _paramanders$elm_twitch_chat$Native_Jsonp = function() {
 
   function jsonp(url, callbackName)
   {
-
     return _elm_lang$core$Native_Scheduler.nativeBinding(function(callback) {
       window[callbackName] = function(content)
       {
         callback(_elm_lang$core$Native_Scheduler.succeed(JSON.stringify(content)));
+        delete window[callbackName];
       };
 
       var scriptTag = createScript(url, callbackName);
